b8293efe3320ee7f86fe0cdbd85abb61b0dee65f
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-01-14 17:49:09 +0300

    adding toaster client

--- airbnb/app/components/modals/RegisterModal.tsx
+++ airbnb/app/components/modals/RegisterModal.tsx
@@ -47,6 +47,8 @@ const RegisterModal = () => {
         <div className='flex flex-col gap-4'>
             <Heading title='Welcome to Airbnb' subtitle='Create an account!' />
             <Input id='email' label='Email' disabled={isLoading} register={register} errors={errors} required />
+            <Input id='name' label='Name' disabled={isLoading} register={register} errors={errors} required />
+            <Input id='password' type="password" label='Password' disabled={isLoading} register={register} errors={errors} required />
         </div>
     )
 
