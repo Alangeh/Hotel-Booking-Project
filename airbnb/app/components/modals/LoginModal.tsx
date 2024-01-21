70bb21e2b4b7b7faab88432bd7e21694aa4c47e1
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-01-21 12:52:09 +0300

    google social login OAuth

--- airbnb/app/components/modals/LoginModal.tsx
+++ airbnb/app/components/modals/LoginModal.tsx
@@ -68,8 +68,8 @@ const LoginModal = () => {
     const footerContent = (
         <div className='flex flex-col gap-4 mt-3'>
             <hr />
-            <Button outline label='Continue with Google' icon={FcGoogle} onclick={() => {}} />
-            <Button outline label='Continue with Github' icon={AiFillGithub} onclick={() => {}} />
+            <Button outline label='Continue with Google' icon={FcGoogle} onclick={() => signIn('google')} />
+            <Button outline label='Continue with Github' icon={AiFillGithub} onclick={() => signIn('github')} />
             <div className='text-neutral-500 text-center mt-4 font-light'>
                 <div className='justify-center flex flex-row items-center gap-2'>
                     <div>
