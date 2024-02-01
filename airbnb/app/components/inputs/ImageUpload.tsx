7160a0f2a4b98d263b02a54260226e5ce3c7fca3
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-02-01 10:38:22 +0300

    Completing image upload module

--- airbnb/app/components/inputs/ImageUpload.tsx
+++ airbnb/app/components/inputs/ImageUpload.tsx
@@ -25,12 +25,34 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     return (
         <CldUploadWidget
             onUpload={handleUpload}
-            uploadPreset=""
+            uploadPreset="vqcp2ekz"
             options={{
                 maxFiles: 1
             }}
         >
-
+            {({ open }) => {
+                return (
+                    <div 
+                        onClick={() => open?.()}
+                        className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
+                    >
+                        <TbPhotoPlus size={50} />
+                        <div className="font-semibold text-lg">
+                            Click to upload
+                        </div>
+                        {value && (
+                            <div className="absolute inset-0 w-full h-full">
+                                <Image
+                                    alt="Upload"
+                                    fill
+                                    style={{ objectFit: 'cover' }}
+                                    src={value}                                                
+                                />
+                            </div>
+                        )}
+                    </div>
+                )
+            }}
         </CldUploadWidget>
     )
 }
