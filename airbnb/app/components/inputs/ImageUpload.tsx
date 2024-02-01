281cc2f973663c900f739b2b38ab50ba03c044ab
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-02-01 09:56:31 +0300

    adding cloudinary

--- airbnb/app/components/inputs/ImageUpload.tsx
+++ airbnb/app/components/inputs/ImageUpload.tsx
@@ -1,10 +1,37 @@
 'use client'
 
-const ImageUpload = () => {
-    return (
-        <div >
+import { CldUploadWidget } from "next-cloudinary";
+import Image from 'next/image';
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
 
-        </div>
+declare global {
+    var cloudinary: any;
+}
+
+interface ImageUploadProps {
+    onChange: (value: string) => void;
+    value: string;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+    onChange,
+    value
+}) => {
+    const handleUpload = useCallback((result: any) => {
+        onChange(result.info.secure_url);
+    },[onChange]);
+
+    return (
+        <CldUploadWidget
+            onUpload={handleUpload}
+            uploadPreset=""
+            options={{
+                maxFiles: 1
+            }}
+        >
+
+        </CldUploadWidget>
     )
 }
 
