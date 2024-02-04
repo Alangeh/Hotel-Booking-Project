e32c8133f62008f6a3ab4734d8df4c523b21d661
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-02-04 19:21:33 +0300

    fetching listing data using prisma

--- airbnb/app/page.tsx
+++ airbnb/app/page.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image'
 import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
 
-  if (isEmpty){
-    return(
+  if (listings.length == 0) {
+    return (
       <ClientOnly>
-        <EmptyState />
+        <EmptyState showReset />
       </ClientOnly>
     )
   }
@@ -17,7 +18,14 @@ export default function Home() {
     <ClientOnly>
       <Container>
         <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-            <div>My future listings</div>
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard 
+                  key={listing.id}
+                  data={listing}
+              />
+            )
+          })}
         </div>
       </Container>
     </ClientOnly>
