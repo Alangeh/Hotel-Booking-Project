8aed30b7fea761cc13c0a1e372cda4feac0ca268
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-02-05 00:07:36 +0300

    setting up the favourites like button

--- airbnb/app/components/listings/ListingCard.tsx
+++ airbnb/app/components/listings/ListingCard.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
 import { format } from 'date-fns';
 import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 interface ListingCardProps {
     data: Listing;
@@ -76,6 +77,12 @@ const ListingCard: React.FC<ListingCardProps> = ({
                         src={data.imageSrc}
                         className="object-cover h-full w-full group-hover:scale-110 transition"
                     />
+                    <div className="absolute top-3 right-3">
+                        <HeartButton 
+                            listingId={data.id}
+                            currentUser={currentUser}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
