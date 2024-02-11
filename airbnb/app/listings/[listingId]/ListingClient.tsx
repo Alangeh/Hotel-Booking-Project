7e27713f16fec7052c3cbf433afe657eb42db78f
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-02-11 19:02:15 +0300

    Listing Client cont..

--- airbnb/app/listings/[listingId]/ListingClient.tsx
+++ airbnb/app/listings/[listingId]/ListingClient.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import Container from "@/app/components/Container";
+import { categories } from "@/app/components/navbar/Categories";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
+import { useMemo } from "react";
 
 
 interface ListingClientProps {
@@ -16,8 +19,23 @@ const ListingClient: React.FC<ListingClientProps> = ({
     listing,
     currentUser
 }) => {
+    const category = useMemo(() => {
+        return categories.find((item) => item.label == listing.category);
+    }, [listing.category]);
     return (
-        <div>Listing Client</div>
+        <Container>
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col gap-6">
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        localtionValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </Container>
     )
 }
 
