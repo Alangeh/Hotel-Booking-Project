346e24b8e52fb134e3c7eaa87531987fa9ed3954
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-02-17 13:17:31 +0300

    Trips Client

--- airbnb/app/trips/TripsClient.tsx
+++ airbnb/app/trips/TripsClient.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Container from "../components/Container";
+import Heading from "../components/Heading";
 import { SafeReservations, SafeUser } from "../types";
 
 interface TripsClientProps {
@@ -12,7 +14,26 @@ const TripsClient: React.FC<TripsClientProps> = ({
     currentUser
 }) => {
     return (
-        <div></div>
+        <Container>
+            <Heading 
+                title="Trips"
+                subtitle="Where you've been and where you're going"
+            />
+            <div className="
+                mt-10 
+                grid 
+                grid-cols-1 
+                sm:grid-cols-2 
+                md:grid-cols-3 
+                lg:grid-cols-4
+                xl:grid-cols-5
+                2xl:grid-cols-6
+                gap-8
+                "
+                >
+
+            </div>
+        </Container>
     )
 }
 
